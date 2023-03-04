afd998a079f579848076d5495a8f127444a7f590
Tonyoffor <tonyc.offor@gmail.com>
2023-03-04 12:40:29 -0500

    heroku deploy

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,26 +13,36 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+  const contentDb = await openDB('jate', 1);
+  const tx = contentDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () =>{
 console.error('getDb not implemented');
 // Create a connection to the database database and version we want to use.
-  const contentDb = await openDB('content', 1);
+  const contentDb = await openDB('jate', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = contentDb.transaction('content', 'readonly');
+  const tx = contentDb.transaction('jate', 'readonly');
 
   // Open up the desired object store.
-  const store = tx.objectStore('content');
+  const store = tx.objectStore('jate');
   
   // Use the .getAll() method to get all data in the database.
-  const request = store.getAll();
+  const request = store.get(1);
 
   // Get confirmation of the request.
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result?.value;
 }
+
+
 initdb();
